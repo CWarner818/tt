a1a81d39d285d32058d573f21a06902fc272b055
Chris Warner <cwarner818@gmail.com>
2018-02-11 06:36:44 -0700

    Take random sampling of tranasactions to check

--- cmd/confirms.go
+++ cmd/confirms.go
@@ -23,21 +23,27 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/cwarner818/giota"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var addresses *[]string
+var (
+	addresses *[]string
+	limit     *int
+)
 
 // confirmsCmd represents the confirms command
 var confirmsCmd = &cobra.Command{
 	Use:   "confirms",
-	Short: "Get transaction confirmation information",
-	Long: `Returns the percentage of confirmed transactions sent to the 
-specified address(es)`,
+	Short: "Returns the percentage of confirmed transactions sent to the specified address(es)",
+	Long: `confirms takes a random sampling of all transactions sent to the specified
+address(es) and then checks to see if they are confirmed according to 
+the latest milestone.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Set the default http client to use
 		httpClient := &http.Client{
@@ -65,8 +71,22 @@ specified address(es)`,
 		if err != nil {
 			log.Fatal("Error getting transaction list: ", err)
 		}
+		fmt.Println(len(txnsResponse.Hashes), "results returned")
 
-		inclusionResponse, err := api.GetInclusionStates(txnsResponse.Hashes,
+		if *limit > len(txnsResponse.Hashes) {
+			*limit = len(txnsResponse.Hashes)
+		}
+		hashes := make([]giota.Trytes, *limit)
+
+		for i := 0; i < *limit; i++ {
+			r := rand.Intn(len(txnsResponse.Hashes))
+			hashes[i] = txnsResponse.Hashes[r]
+			txnsResponse.Hashes = append(txnsResponse.Hashes[:r],
+				txnsResponse.Hashes[r+1:]...)
+		}
+
+		//pp.Print(txnsResponse.Hashes[:5])
+		inclusionResponse, err := api.GetInclusionStates(txnsResponse.Hashes[:*limit],
 			[]giota.Trytes{milestone})
 
 		if err != nil {
@@ -118,5 +138,9 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
+	limit = confirmsCmd.Flags().IntP("limit", "l", 500, "Limit checking to this number of transactions")
+
 	addresses = confirmsCmd.Flags().StringSliceP("address", "a", nil, "Address to get confirmation information for")
+
+	rand.Seed(time.Now().UnixNano())
 }
